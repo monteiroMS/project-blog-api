889c4cfc0ed2d95b42471f119a6662a1462f9ea5
Matheus Monteiro <matheuschran@hotmail.com>
2022-08-30 21:50:41 +0000

    Corrige erro de lint

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,7 +11,7 @@ const authenticator = async (req, res, next) => {
 
     if (!authorization) {
       return res.status(401).json({ message: NOT_FOUND_MESSAGE });
-    };
+    }
 
     const { email } = jwt.verify(authorization, secret);
     const { id } = await User.findOne({ where: { email } });

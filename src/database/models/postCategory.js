ad6a55c0a88fe3e3a181d08dd6b0ecc162cf0703
Matheus Monteiro <matheuschran@hotmail.com>
2022-09-03 15:25:19 +0000

    Requisito 11 - Cria model PostCategory com suas associacoes

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
+    { timestamps: false, tablename: 'PostCategories' },
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
